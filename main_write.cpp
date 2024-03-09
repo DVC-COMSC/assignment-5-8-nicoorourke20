4aff1e71c74468cbf7d77a85051d7dc463c7156c
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 21:50:25 -0800

    Update main_write.cpp

--- main_write.cpp
+++ main_write.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+int main() {
+    ofstream outFile("students.txt");
+
+    if (!outFile) {
+        cerr << "Failed to open file." << endl;
+        return 1;
+    }
+
+    int numStudents;
+    cout << "Enter the number of students: ";
+    cin >> numStudents;
+    outFile << numStudents << endl;
+
+    for (int i = 0; i < numStudents; ++i) {
+        string name;
+        int score1, score2;
+
+        cout << "Enter student name, score1, and score2 for student " << i + 1 << ": ";
+        cin >> name >> score1 >> score2;
+
+        outFile << name << " " << score1 << " " << score2 << endl;
+    }
+    outFile.close();
+    return 0;
 }
