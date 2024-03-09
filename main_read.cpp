67c0bd994c08e469ef43c3c232c00cd515f6c654
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 21:50:54 -0800

    Update main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+int main() {
+    ifstream inFile("students.txt");
+
+    if (!inFile) {
+        cerr << "Failed to open file." << endl;
+        return 1;
+    }
+
+    int numStudents;
+    inFile >> numStudents;
+    cout << "Number of students: " << numStudents << endl;
+
+    for (int i = 0; i < numStudents; ++i) {
+        string name;
+        int score1, score2;
+
+        inFile >> name >> score1 >> score2;
+        int sum = score1 + score2;
+        float average = sum / 2.0f;
+
+        cout << "Student Name: " << name << "\tscore1: " << score1 << "\tscore2: " << score2
+             << "\tSum: " << sum << "\tAvg: " << average << endl;
+    }
+    inFile.close();
+    return 0;
 }
